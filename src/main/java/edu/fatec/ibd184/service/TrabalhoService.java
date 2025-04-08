3ffd0379504ded5cd9440fad129aa80bc812b3cb
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-04-08 15:21:07 -0300

    Create TrabalhoService with the novo method to handle saving

---
+++ src/main/java/edu/fatec/ibd184/service/TrabalhoService.java
@@ -0,0 +1,28 @@
+package edu.fatec.ibd184.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import edu.fatec.ibd184.entity.Trabalho;
+import edu.fatec.ibd184.reporitory.TrabalhoRepository;
+
+@Service
+public class TrabalhoService {
+
+    @Autowired
+    private TrabalhoRepository repo;
+
+    public Trabalho novo(Trabalho trabalho)
+    {
+        if (trabalho.getTitulo() == null ||
+                trabalho.getTitulo().isBlank() ||
+                trabalho.getGrupo() == null ||
+                trabalho.getGrupo().isBlank())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Titulo ou grupo vazio");
+
+        return repo.save(trabalho);
+    }
+    
+}
