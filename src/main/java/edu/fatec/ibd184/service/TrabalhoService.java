bb7792e321a9faef94f4966182744947bb2ce944
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-04-08 15:32:34 -0300

    Add TrabalhoService.findAll method

--- src/main/java/edu/fatec/ibd184/service/TrabalhoService.java
+++ src/main/java/edu/fatec/ibd184/service/TrabalhoService.java
@@ -1,5 +1,7 @@
 package edu.fatec.ibd184.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,11 @@ public class TrabalhoService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Titulo ou grupo vazio");
 
         return repo.save(trabalho);
+    }
+
+    public List<Trabalho> findAll()
+    {
+        return repo.findAll();
     }
     
 }
